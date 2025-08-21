08f45316acae565eef73c711e41fa7daec6ab5e9
Shakti Priya <priyashakti68@gmail.com>
2025-08-21 22:59:38 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -10,7 +10,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/shaktipriya13",
   resume:
-    "https://drive.google.com/file/d/1ZkaIPy7DvEWlhCb4-klxMyTPlOt46fAa/view?usp=sharing",
+    "https://drive.google.com/file/d/1H3ioj2WqEg8J6XyZvriHO_wdym9S1lkW/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/shaktipriya13/",
   // twitter: "https://twitter.com/RishavChanda",
   // insta: "https://www.instagram.com/rishav_chanda/",
